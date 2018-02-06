6124056b2e0e1dd5ea30fa32469519044ba4a23a
OnurKadem <onurkadem@web.de>
2018-02-06 15:23:39 +0100

    First Test

--- app/src/main/java/com/example/onur/myapplication/MainActivity.java
+++ app/src/main/java/com/example/onur/myapplication/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.onur.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addButton = (Button) findViewById(R.id.AddButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText firstNumEditText = findViewById(R.id.firstNumEdit);
+                EditText secondNumEditText = findViewById(R.id.secondNumEdit);
+                TextView resultTextView = findViewById(R.id.textViewResult);
+
+                int num1 = Integer.parseInt(firstNumEditText.getText().toString());
+                int num2 = Integer.parseInt(secondNumEditText.getText().toString());
+                int result = num1 + num2;
+                resultTextView.setText(result + "");
+            }
+        });
     }
 }
